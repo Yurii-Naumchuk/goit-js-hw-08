a1ce1402843fe026615d9c77b336e5b51ca05496
Yurii-Naumchuk <98982620+Yurii-Naumchuk@users.noreply.github.com>
2022-08-10 00:18:21 +0300

    3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+function createImageGallery(itemList) {
+    return itemList.reduce((acc, item) => acc + 
+    `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`
+      , "");
+    };
 
-console.log(galleryItems);
+    const createImg = createImageGallery(galleryItems);
+    const listGallery = document.querySelector(".gallery");
+  
+    listGallery.insertAdjacentHTML("beforeend", createImg);
+
+    const lightbox = new SimpleLightbox('.gallery a', {
+        
+        captionSelector: "img",
+        captionsData: "alt",
+        captionPosition: "bottom",
+        captionDelay: 250, });
+
