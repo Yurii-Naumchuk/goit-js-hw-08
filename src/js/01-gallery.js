6bc8dc13ae933314b4a21e11b80023b3b77af5f6
Yurii-Naumchuk <98982620+Yurii-Naumchuk@users.noreply.github.com>
2022-08-11 01:27:41 +0300

    4

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,9 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 function createImageGallery(itemList) {
     return itemList.reduce((acc, item) => acc + 
     `<a class="gallery__item" href="${item.original}">
