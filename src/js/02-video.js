ca01cbe8a910ae3ca447db23a7062b381872e73d
Yurii-Naumchuk <uranaumchuk7@gmail.com>
2022-08-18 22:23:31 +0300

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,15 @@
-import Vimeo from '@vimeo/player';
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-
-
 const iframe = document.querySelector('iframe');
-const player = new Vimeo(iframe);
+const player = new Player(iframe);
 
-const onPlayer = function (data) {
-    const playedTime = data.seconds;
-    localStorage.setItem("videoplayer-current-time", playedTime)
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
 }
 
-const savePletedTime = localStorage.getItem("videoplayer-current-time"); 
-
-
-player.on('timeupdate', throttle(onPlayer, 1000));
-if(savePletedTime) {
-    player.setCurrentTime(savePletedTime);
+if(localStorage.getItem('videoplayer-current-time')) {
+        player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 }
+
+player.on('timeupdate', throttle(onPlay, 1000));
